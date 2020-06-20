2d840a4d1b53e7f1ac351393c69d7846267e290b
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-06-19 23:08:25 -0500

    fix errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from '@reach/router'
 
 export const Header = ({ darkMode, handleClick }) => {
   return (
     <header className='header'>
       <div className='container'>
-        <h1 className='header__title'>Where in the world?</h1>
-        <div className='header__button' onClick={handleClick}>
+        <Link to='/'>
+          <h1 className='header__title'>Where in the world?</h1>
+        </Link>
+        <p className='header__button' onClick={handleClick}>
           <ion-icon name={`moon-${darkMode ? 'sharp' : 'outline'}`} />
           <span> Dark Mode</span>
-        </div>
+        </p>
       </div>
     </header>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Countries } from './Countries'
 
-export const Main = () => {
+export const Main = ({ data, handleSearch, handleFilter }) => {
   return (
     <main className='main'>
       <div className='container'>
         <div className='main__input'>
           <ion-icon name='search-sharp' />
-          <input type='text' placeholder='Search for a country...' />
+          <input type='text' placeholder='Search for a country...' onChange={handleSearch} />
         </div>
-        <select className='main__filter' defaultValue='default' name='filter'>
-          <option value='default' disabled>Filter by Region</option>
+        <select className='main__filter' defaultValue='default' name='filter' onChange={handleFilter}>
+          <option value='default' className='filter__option--disabled'>Filter by Region</option>
           <option value='africa'>Africa</option>
-          <option value='america'>America</option>
+          <option value='americas'>America</option>
           <option value='asia'>Asia</option>
           <option value='europe'>Europe</option>
-          <option value='oceanía'>Oceanía</option>
+          <option value='oceania'>Oceanía</option>
         </select>
       </div>
-      <Countries />
+      <Countries data={data} />
     </main>
   )
 }
