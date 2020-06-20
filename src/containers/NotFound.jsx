eb1e13bf34b682e73a8a846b97e0e60e93f54a17
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-06-19 23:11:35 -0500

    Not found page created

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NotFound = () => {
+  return (
+    <section className='notFound'>
+      <div className='container'>
+        <h1>404</h1>
+        <p>NOT FOUND</p>
+      </div>
+    </section>
+  )
+}
