a5997bebf551dd0f0e02358e9f352d98ee92dc64
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-06-19 23:10:04 -0500

    countries component created

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Country } from './Country'
 
-export const Countries = () => {
+export const Countries = ({ data }) => {
   return (
     <div className='countries container'>
-      <Country />
+      {data.length
+        ? data.map(country => {
+          return (
+            <Country
+              key={country.name}
+              name={country.name}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+              flag={country.flag}
+            />
+          )
+        })
+        : 'No country found...'}
     </div>
   )
 }
